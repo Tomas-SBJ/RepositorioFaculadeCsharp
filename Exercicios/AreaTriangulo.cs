aa1b75662183a09232c3cb90890b2604c3fa95ed
Tomás Schaefer Borges de Jesus <tomassbj@gmail.com>
2021-08-06 13:28:32 -0300

    Criação da atividade de calcular area do triangulo.

---
+++ Exercicios/AreaTriangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicios
+{
+     public static class AreaTriangulo
+     {
+        static void Main(string[] args)
+        {
+            int baseTriangulo;
+            int alturaTriangulo;
+            int areaTriangulo;
+
+            Console.WriteLine("Digite o valor da base:");
+            baseTriangulo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da altura:");
+            alturaTriangulo = Convert.ToInt32(Console.ReadLine());
+
+            areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+            Console.WriteLine("Área do triângulo: " + areaTriangulo);
+        }
+     }
+}
