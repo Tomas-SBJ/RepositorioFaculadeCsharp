58b1f364ff43aa034d1fa3b0cc42f55e297b344c
Tomás Schaefer Borges de Jesus <tomassbj@gmail.com>
2021-08-06 13:35:41 -0300

    Criação atividade de calcular idade aproximada.

---
+++ CalculoIdade/CalculoIdade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculoIdade
+{
+    public static class CalculoIdade
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento;
+            int anoAtual = DateTime.Now.Year;
+            int idade;
+
+            Console.WriteLine("Digite seu ano de nascimento:");
+            anoNascimento = Convert.ToInt32(Console.ReadLine());
+
+            if (anoNascimento > anoAtual)
+            {
+                throw new ArgumentException("Ano de nascimento inválido.");
+            }
+
+            idade = anoAtual - anoNascimento;
+
+            Console.WriteLine("Sua idade aproximada: " + idade);
+        }
+    }
+}
