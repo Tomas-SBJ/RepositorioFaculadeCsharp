911552dd9fa2ae5e280a93ed9c1cc74d71d723a2
Tomás Schaefer Borges de Jesus <tomassbj@gmail.com>
2021-08-06 14:09:15 -0300

    Criação de LeitorTresNumeros e FichaFuncionario

---
+++ FichaFuncionario/FichaFuncionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FichaFuncionario
+{
+    public static class FichaFuncionario
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double horasTrabalhadas;
+            int dependetes;
+            double valorHora = 32.00;
+            double valorDependente = 40.00;
+            double inss;
+            double ir;
+            double salarioLiquido;
+            double salarioBruto;
+
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite a quantidade de horas trabalhadas:");
+            horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de dependentes:");
+            dependetes = Convert.ToInt32(Console.ReadLine());
+
+            salarioBruto = (horasTrabalhadas * valorHora) + (valorDependente * dependetes);
+            inss = salarioBruto * 0.085;
+            ir = salarioBruto * 0.05;
+            salarioLiquido = salarioBruto - inss - ir;
+
+            Console.WriteLine("Boa tarde " + nome);
+            Console.WriteLine("Salario bruto: " + salarioBruto);
+            Console.WriteLine("Desconto do INSS: " + inss);
+            Console.WriteLine("Desconto do IR: " + ir);
+            Console.WriteLine("Salario liquido: " + salarioLiquido);
+        }
+    }
+}
---
+++ LeitorTresNumeros/LeitorTresNumeros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeitorTresNumeros
+{
+    public static class LeitorTresNumeros
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+
+            Console.WriteLine("Digite seu primeiro número:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu primeiro número:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu primeiro número:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Resultado da operação (V1 + V2): " + (num1 + num2));
+            Console.WriteLine("Resultado da operação (V2 - V1): " + (num2 - num1));
+            Console.WriteLine("Resultado da operação (V3 * V1): " + (num3 * num1));
+            Console.WriteLine("Resultado da operação (V3 + V2): " + (num3 + num2));
+            Console.WriteLine("Resultado da operação (V3 - V2): " + (num3 - num2));
+            Console.WriteLine("Resultado da operação (V1 + V2 + V3): " + (num1 + num2 + num3));
+        }
+    }
+}
