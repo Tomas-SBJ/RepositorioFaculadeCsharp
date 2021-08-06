6f50f15d0ca8726bb1b5e0d18cefedcacca274f7
Tomás Schaefer Borges de Jesus <tomassbj@gmail.com>
2021-08-06 14:25:06 -0300

    Finalização dos exercicios para entrega no dia 01/05/2021

---
+++ MultiplicacaoNumeros/MultiplicacaoNumeros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MultiplicacaoNumeros
+{
+    public static class MultiplicacaoNumeros
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            double resultado;
+
+            Console.WriteLine("Digite seu primeiro número:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite seu segundo número:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = num1 * num2;
+
+            Console.WriteLine("Resultado de sua multiplicação: " + resultado);
+        }
+    }
+}
---
+++ QuatroOperacoes/QuatroOperacoes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace QuatroOperacoes
+{
+    public static class QuatroOperacoes
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int resultadoSoma;
+            int resultadoSubtracao;
+            int resultadoMultiplicacao;
+            double resultadoDivisao;
+
+            Console.WriteLine("Digite seu primeiro número:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite seu segundo número:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            resultadoSoma = num1 + num2;
+            resultadoSubtracao = num1 - num2;
+            resultadoMultiplicacao = num1 * num2;
+
+            Console.WriteLine("Resultado da soma: " + resultadoSoma);
+            Console.WriteLine("Resultado da subtração: " + resultadoSubtracao);
+            Console.WriteLine("Resultado da multiplicação: " + resultadoMultiplicacao);
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("Número 0 inválido para divisão");
+            }
+            else
+            {
+                resultadoDivisao = (double)num1 / num2;
+                Console.WriteLine("Resultado da divisão: " + resultadoDivisao);
+            }
+        }
+    }
+}
